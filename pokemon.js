e7c5364ff19546a4e0208983a64b451870e59aa2
Alexander Little <38229804+thealexlittle@users.noreply.github.com>
2019-03-04 16:23:33 -0800

    Update pokemon.js
    Everything passed the sample test except for the double quotes.

--- pokemon.js
+++ pokemon.js
@@ -1,1 +1,27 @@
-
+function Pokemon (name, attack, defense, health, type) {
+	this.name = name;
+	this.attack = attack;
+	this.defense = defense;
+	this.health = health;
+	this.initialthealth = health;
+	this.type = type;
+	
+	this.takeDamage = function (damage){
+		if (this.health < damage) {
+			this.health = 0;
+		}	
+		else{ 
+			this.health -= damage;
+		}
+	};
+	
+	this.attackOpponent =  function (pokemon){
+	damage = this.attack - pokemon.defense;
+	pokemon.takeDamage(damage);
+	};
+  
+	this.display = function () {
+	display = this.name.toUpperCase() + "(" +this.type.toUpperCase() + ")" + this.currenthealth+ "/" + this.health;
+		return display;
+	}
+}	
