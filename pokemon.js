a709d5f36e57425d9206d18152d5dbcbbd2d4927
Alexander Little <38229804+thealexlittle@users.noreply.github.com>
2019-03-04 18:06:31 -0800

    Update pokemon.js
    fixed global variable leak. It should be all good chief

--- pokemon.js
+++ pokemon.js
@@ -3,9 +3,9 @@ function Pokemon (name, attack, defense, health, type) {
 	this.attack = attack;
 	this.defense = defense;
 	this.health = health;
-	this.initialthealth = health;
+	this.initialhealth = health;
 	this.type = type;
-	
+	this.dam = 0;
 	this.takeDamage = function (damage){
 		if (this.health < damage) {
 			this.health = 0;
@@ -16,12 +16,11 @@ function Pokemon (name, attack, defense, health, type) {
 	};
 	
 	this.attackOpponent =  function (pokemon){
-	damage = this.attack - pokemon.defense;
-	pokemon.takeDamage(damage);
+	this.dam = this.attack - pokemon.defense;
+	pokemon.takeDamage(this.dam);
 	};
   
 	this.display = function () {
-	display = this.name.toUpperCase() + "(" +this.type.toUpperCase() + ")" + this.currenthealth+ "/" + this.health;
-		return display;
+		return this.name.toUpperCase() + " (" +this.type.toUpperCase() + ") " + this.health + "/" + this.initialhealth;
 	}
 }	
