7ecdd9f56aa71d2431064bda635955bde4150fe0
Alexander Little <38229804+thealexlittle@users.noreply.github.com>
2019-03-04 21:42:38 -0800

    Update pokemon.js
    Finished Project

--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,17 @@
 function Pokemon (name, attack, defense, health, type) {
+	//variable constructors 
+	
 	this.name = name;
 	this.attack = attack;
 	this.defense = defense;
 	this.health = health;
 	this.initialhealth = health;
 	this.type = type;
-	this.dam = 0;
+
+	/*takeDamage() method that takes in a damage value and 
+	 subtracts it from the pokemon's health value. If the 
+	 damage is greatter than the health of the pokemon, damage 
+	 is set to 0 */
 	this.takeDamage = function (damage){
 		if (this.health < damage) {
 			this.health = 0;
@@ -13,14 +19,18 @@ function Pokemon (name, attack, defense, health, type) {
 		else{ 
 			this.health -= damage;
 		}
-	};
+	}
 	
+	/* attackOpponent() method  calculates the damage done to 
+	the target pokemon and calls takeDamage() to apply that 
+	damage to the pokemon*/
 	this.attackOpponent =  function (pokemon){
-	this.dam = this.attack - pokemon.defense;
-	pokemon.takeDamage(this.dam);
-	};
-  
+		pokemon.takeDamage(this.attack - pokemon.defense);
+	}
+	
+	/* Displays the name, type, and health of the pokemon */
 	this.display = function () {
-		return this.name.toUpperCase() + " (" +this.type.toUpperCase() + ") " + this.health + "/" + this.initialhealth;
+		return this.name.toUpperCase() + " (" +this.type.toUpperCase() + 
+		") " + this.health + "/" + this.initialhealth;
 	}
 }	
